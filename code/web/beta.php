88ffcaabbec24b1c60d37d16fdd0aad1c2e9d434
Aditya Relangi <arelangi@gmail.com>
2013-11-23 12:20:16 -0600

    site changes

--- code/web/beta.php
+++ code/web/beta.php
@@ -2,7 +2,7 @@
 
 	$myFile = "beta.txt";
 	$fh = fopen($myFile, 'a') or die("can't open file");
-	$stringData = $_GET['email'];
+	$stringData = $_GET['email']."\n";
 	fwrite($fh, $stringData);
 	fclose($fh);
 
