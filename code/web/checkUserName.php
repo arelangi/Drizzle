f61447200ce3899377c218483e4d424f78153183
Aditya Relangi <arelangi@gmail.com>
2014-01-23 21:01:30 -0600

    closes #12

---
+++ code/web/checkUserName.php
@@ -0,0 +1,14 @@
+<?php 
+
+	mysql_connect('127.0.0.1','developer','developer')or die(mysql_error());
+    mysql_select_db('drizzle')  or die(mysql_error());
+
+    $query = "select * from users where username='".mysql_real_escape_string($_GET["username"])."';";
+    $res = mysql_query($query);
+    if(mysql_num_rows($res)>0){
+		echo json_encode(array("available"=>"no"));
+	}else{
+		echo json_encode(array("available"=>"yes"));
+	}
+	
+?>
